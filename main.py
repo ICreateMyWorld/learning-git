9bb32de8970198954132d928cde1d8da526c122d
Maja Jablonska <mjalbatros82@gmail.com>
2022-02-17 21:23:28 +0100

    Add main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+shopping_list={"piekarnia" : ["chleb","paczek","buleczki"],
+"warzywniak":["marchew","seler","rukola","kolendra"]}
+
+total_item_count = 0
+for store, items in shopping_list.items():
+    items_as_str = ', '.join(items)
+    print(f"Idę do {store.capitalize()}, kupuję tu następujące rzeczy: {items_as_str.title()}")
+    total_item_count += len(items)
+print(f"W sumie kupuję {total_item_count} produktów")
